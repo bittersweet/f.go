b2f5b6fa198c1050bd9db5ecd88784f979d5f41a
Mark Mulder <markmulder@gmail.com>
2013-09-12 15:19:33 +0200

    Colorize result

--- ff.go
+++ ff.go
@@ -9,7 +9,16 @@ import (
 )
 
 func visit(path string, f os.FileInfo, err error) error {
+	red := "\033[31m"
+	reset := "\033[0m"
+
 	if path == "." {
+		return nil
+	}
+
+	// Ignore .git and tmp directories
+	ignore, _ := regexp.Compile(".git|tmp")
+	if ignore.MatchString(path) == true {
 		return nil
 	}
 
@@ -21,7 +30,9 @@ func visit(path string, f os.FileInfo, err error) error {
 	}
 
 	if r.MatchString(path) == true {
-		fmt.Println(path)
+		replaceWith := fmt.Sprintf("%v%v%v", red, flag.Arg(0), reset)
+		formattedResult := r.ReplaceAllString(path, replaceWith)
+		fmt.Println(formattedResult)
 	}
 
 	return nil
